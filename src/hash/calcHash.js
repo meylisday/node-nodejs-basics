68f340760b95aab55b79b75b97f493c5e0e61541
meylisday <victory.sett@gmail.com>
2023-06-16 14:16:17 +0200

    Add calc hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import crypto from "crypto";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const currentModulePath = fileURLToPath(import.meta.url);
+  const currentDirPath = dirname(currentModulePath);
+  const sourceFolderPath = path.join(currentDirPath, "files");
+  const filePath = path.join(sourceFolderPath, "fileToCalculateHashFor.txt");
+  const fileBuffer = fs.readFileSync(filePath);
+  const hashSum = crypto.createHash("sha256");
+  hashSum.update(fileBuffer);
+
+  const hex = hashSum.digest("hex");
+
+  console.log(hex);
 };
 
 await calculateHash();
