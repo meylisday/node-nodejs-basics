61f410e038c5c9a1eaa75d004e60f17794d29000
meylisday <victory.sett@gmail.com>
2023-06-16 13:20:13 +0200

    Add tasks Command line interface

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith(prefix)) {
+      const variableName = key.slice(prefix.length);
+      console.log(`RSS_${variableName}=${value}`);
+    }
+  }
 };
 
 parseEnv();
