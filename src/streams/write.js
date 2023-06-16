e095a628785cb4ff093ebe6bb59f8e32b37e8f69
meylisday <victory.sett@gmail.com>
2023-06-16 16:52:13 +0200

    Streams: write  task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
 const write = async () => {
-    // Write your code here 
+  const currentModulePath = fileURLToPath(import.meta.url);
+  const currentDirPath = dirname(currentModulePath);
+  const sourceFolderPath = path.join(currentDirPath, "files");
+  const filePath = path.join(sourceFolderPath, "fileToWrite.txt");
+  const writableStream = fs.createWriteStream(filePath, "utf8");
+  process.stdin.on("data", (data) => {
+    writableStream.write(data);
+    process.exit();
+  });
 };
 
 await write();
