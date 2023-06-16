6e2dbbe839141420003ee96404a6998d73c90fe8
meylisday <victory.sett@gmail.com>
2023-06-16 20:47:12 +0200

    Zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import path from "path";
+
 const compress = async () => {
-    // Write your code here 
+    const currentDirPath = path.dirname(new URL(import.meta.url).pathname);
+    const filePath = path.join(
+      path.join(currentDirPath, "files"),
+      "fileToCompress.txt"
+    );
+    const compressFile = (file) => {
+      const handleStream = createReadStream(file)
+      handleStream
+        .pipe(createGzip())
+        .pipe(createWriteStream(path.join(path.join(currentDirPath, "files"),
+            "archive.gz")))
+        .on('finish', () => {
+          console.log(`Compression process done: ${file}`)
+        });
+    };
+    compressFile(filePath);
 };
 
 await compress();
