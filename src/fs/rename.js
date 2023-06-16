a63fbeb0b125fcb3e76f7baa4a32a00b8745a0d3
meylisday <victory.sett@gmail.com>
2023-06-16 09:41:17 +0200

    File system: rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,42 @@
+import { promises as fsPromises, constants as fsConstants } from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const currentModulePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentModulePath);
+const sourceFolderPath = path.join(currentDirPath, "files");
+const TxtFilePath = path.join(sourceFolderPath, "wrongFilename.txt");
+const MdFilePath = path.join(sourceFolderPath, "properFilename.md");
+
+async function checkMissingTxtFile() {
+  try {
+    await fsPromises.access(TxtFilePath, fsConstants.R_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function checkExistsMdFile() {
+  try {
+    await fsPromises.access(MdFilePath, fsConstants.F_OK);
+    return false;
+  } catch (error) {
+    return true;
+  }
+}
+
 const rename = async () => {
-    // Write your code here 
+  if ((await checkMissingTxtFile()) && (await checkExistsMdFile())) {
+    try {
+      await fsPromises.rename(TxtFilePath, MdFilePath);
+      console.log("File renamed successfully!");
+    } catch (error) {
+      console.log(error.message);
+    }
+  } else {
+    console.log("FS operation failed");
+  }
 };
 
 await rename();
