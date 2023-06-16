418dadfcb99484002e4f205cf31225a3fdd42582
meylisday <victory.sett@gmail.com>
2023-06-16 09:15:13 +0200

    File system: create, copy tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,48 @@
+import { promises as fsPromises, constants as fsConstants } from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const currentModulePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentModulePath);
+const sourceFolderPath = path.join(currentDirPath, "files");
+const destinationFolderPath = path.join(currentDirPath, "files_copy");
+
+async function checkSourceFolderAccess() {
+  try {
+    await fsPromises.access(sourceFolderPath, fsConstants.R_OK);
+    return true;
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  }
+}
+
+async function checkDestinationFolder() {
+  try {
+    await fsPromises.access(destinationFolderPath, fsConstants.F_OK);
+    return false;
+  } catch (error) {
+    return true;
+  }
+}
+
 const copy = async () => {
-    // Write your code here 
+  if ((await checkSourceFolderAccess()) && (await checkDestinationFolder())) {
+    try {
+      await fsPromises.mkdir(destinationFolderPath);
+      const files = await fsPromises.readdir(sourceFolderPath);
+      for (const file of files) {
+        const sourceFilePath = path.join(sourceFolderPath, file);
+        const destinationFilePath = path.join(destinationFolderPath, file);
+        await fsPromises.copyFile(sourceFilePath, destinationFilePath);
+      }
+      console.log("Files folder copied successfully!");
+    } catch (error) {
+      console.log(error.message);
+    }
+  } else {
+    console.log("FS operation failed");
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const folderPath = path.join(
+    path.dirname(new URL(import.meta.url).pathname),
+    "files"
+  );
+  const filePath = path.join(folderPath, "fresh.txt");
+
+  if (fs.existsSync(filePath)) {
+    throw new Error("FS operation failed");
+  }
+
+  fs.writeFileSync(filePath, "I am fresh and young");
+
+  console.log("Fresh file created successfully!");
 };
 
 await create();
